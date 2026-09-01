5383b0e8e245fabd343f8ddead4c76961dfedb77
August Hauge Smevold <august.hauge.smevold@gmail.com>
2026-09-01 12:07:03 +0200

    Can now read .mat files and created time array

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -1,6 +1,16 @@
-from os.path import dirname, join as pjoin # pjoin kombinerer to eller flere filstier, for å slippe å skrive full filsti hver gang
-import scipy.io as sio
-import h5py
+import numpy as np
+import scipy.io as sio # Scipy input output har funksjon for lesing av .mat-filer
+import matplotlib.pyplot as plt
 
-mat_dir = pjoin()
+# Trenger ikke .mat i filnavnet
+data = sio.loadmat('HelikopterParakopter/Verdier_lab_1_v1') # Laster inn dataen fra stien
 
+# print("simout data:\n", data['simout']) # Det er kun Key: 'simout' som er relevant å se på
+
+# data['simout'] ser slik ut: [0: travel, 1: travel_rate, 2: pitch, 3: pitch_rate, 4: elevation, 5: elevation_rate]
+
+Ts = 0.002 # Timestep
+
+## Lager tidsarray
+sim_array_length = int(np.size(data['simout']) / np.size(data['simout'][0])) # Størrelsen på hele array delt på størrelsen på ett element
+t = np.arange(0, sim_array_length, Ts) # Array med samme lengde som simout, med riktig timestep
