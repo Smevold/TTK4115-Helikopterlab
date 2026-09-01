16d095497847bd5a91650fcdabfd25e240ca7c36
August Hauge Smevold <august.hauge.smevold@gmail.com>
2026-09-01 11:05:25 +0200

    Added h5py to uv project

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -1,2 +1,6 @@
-import scipy
+from os.path import dirname, join as pjoin # pjoin kombinerer to eller flere filstier, for å slippe å skrive full filsti hver gang
+import scipy.io as sio
+import h5py
+
+mat_dir = pjoin()
 
